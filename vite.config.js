9873cca24471184a29ee01c7c62e1cdb2c562dbd
Klara <klarakucerova09@gmail.com>
2025-10-07 16:38:40 +0800

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            '@images': path.resolve(__dirname, 'images'),
-        }
+            '@images': path.resolve(__dirname, 'src/images')
+        },
+    },
+    build: {
+        outDir: 'dist',
     },
 })
+
